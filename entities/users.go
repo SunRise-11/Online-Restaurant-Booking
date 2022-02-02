f5229fcb909efb9778f89f0d22795b7e93d5e5fc
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-02-02 15:39:03 +0800

    entity user

--- entities/users.go
+++ entities/users.go
@@ -1,1 +1,13 @@
 package entities
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Email        string
+	Password     string
+	Name         string
+	Phone_number string
+	Reputation   int
+	Balance      int
+}
