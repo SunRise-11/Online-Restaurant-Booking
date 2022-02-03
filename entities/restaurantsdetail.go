0483f3f2c5ee033fca732a48e8fa6a0183994ddd
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-02-03 17:06:58 +0800

    entities restaurant detail, from time to string data type

--- entities/restaurantsdetail.go
+++ entities/restaurantsdetail.go
@@ -1,25 +1,24 @@
 package entities
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type RestaurantDetail struct {
 	gorm.Model
-	ID             uint
-	Name           string
-	Open           time.Time
-	Close          time.Time
-	Price          int
-	Latitude       float64
-	Longitude      float64
-	City           string
-	Address        string
-	PhoneNumber    string
-	ProfilePicture string
-	Seats          int
-	Status         string `gorm:"default:close"`
-	Description    string
+	ID              uint
+	Name            string
+	OperationalHour string
+	Open            string
+	Close           string
+	Price           int
+	Latitude        float64
+	Longitude       float64
+	City            string
+	Address         string
+	PhoneNumber     string
+	ProfilePicture  string
+	Seats           int
+	Status          string `gorm:"default:close"`
+	Description     string
 }
