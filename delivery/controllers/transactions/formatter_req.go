da89f6e1506dec1b616e324dbd02b94681fa180c
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-02-03 15:40:05 +0700

    fix feature get transactions

--- delivery/controllers/transactions/formatter_req.go
+++ delivery/controllers/transactions/formatter_req.go
@@ -1,9 +1,5 @@
 package transactions
 
-type LoginRequestFormat struct {
-	Email    string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
-}
 type TransactionRequestFormat struct {
 	UserID       uint   `json:"user_id" form:"user_id"`
 	RestaurantID uint   `json:"restaurant_id" form:"restaurant_id"`
--- repository/transactions/transactions.go
+++ repository/transactions/transactions.go
@@ -26,7 +26,7 @@ func (tr *TransactionRepository) Create(newTransaction entities.Transaction) (en
 func (tr *TransactionRepository) GetAllWaiting(userId uint) ([]entities.Transaction, error) {
 	transaction := []entities.Transaction{}
 
-	if err := tr.db.Find(&transaction).Where("user_id=? and status=waiting for confirmation", userId).Error; err != nil {
+	if err := tr.db.Where("user_id=? and status=?", userId, "waiting for confirmation").Find(&transaction).Error; err != nil {
 		return transaction, err
 	}
 	return transaction, nil
@@ -34,7 +34,7 @@ func (tr *TransactionRepository) GetAllWaiting(userId uint) ([]entities.Transact
 func (tr *TransactionRepository) GetHistory(userId uint) ([]entities.Transaction, error) {
 	transaction := []entities.Transaction{}
 
-	if err := tr.db.Find(&transaction).Where("user_id=? and status in ('Success','Fail','Cancel','Rejected'", userId).Error; err != nil {
+	if err := tr.db.Where("user_id=?", userId).Where("status in ?", []string{"Success", "Fail", "Cancel", "Rejected"}).Find(&transaction).Error; err != nil {
 		return transaction, err
 	}
 	return transaction, nil
@@ -42,7 +42,7 @@ func (tr *TransactionRepository) GetHistory(userId uint) ([]entities.Transaction
 func (tr *TransactionRepository) GetAllAppointed(userId uint) ([]entities.Transaction, error) {
 	transaction := []entities.Transaction{}
 
-	if err := tr.db.Find(&transaction).Where("user_id=? and status='Accepted' )", userId).Error; err != nil {
+	if err := tr.db.Where("user_id=? and status=? ", userId, "Accepted").Find(&transaction).Error; err != nil {
 		return transaction, err
 	}
 	return transaction, nil
