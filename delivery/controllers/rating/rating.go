4509b8909e0bf8777f1f58d394e0b598091a6cf4
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-02-04 15:45:20 +0800

    routes for rating

--- delivery/controllers/rating/rating.go
+++ delivery/controllers/rating/rating.go
@@ -19,104 +19,110 @@ func NewRatingController(ratrep rating.RatingInterface) *RatingController {
 	return &RatingController{Repo: ratrep}
 }
 
-func (rc RatingController) Create(c echo.Context) error {
-	var ratingRequest PostRatingRequest
+func (rc RatingController) Create() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var ratingRequest PostRatingRequest
 
-	if err := c.Bind(&ratingRequest); err != nil {
-		return c.JSON(http.StatusBadRequest, common.NewBadRequestResponse())
+		if err := c.Bind(&ratingRequest); err != nil {
+			return c.JSON(http.StatusBadRequest, common.NewBadRequestResponse())
+		}
+
+		uid := c.Get("user").(*jwt.Token)
+		claims := uid.Claims.(jwt.MapClaims)
+		userID := int(claims["userid"].(float64))
+
+		isCanGiveRating, _ := rc.Repo.IsCanGiveRating(userID, ratingRequest.RestaurantID)
+		if !isCanGiveRating {
+			return c.JSON(http.StatusNotFound, common.NewNotFoundResponse())
+		}
+
+		data := entities.Rating{
+			RestaurantID: uint(ratingRequest.RestaurantID),
+			UserID:       uint(userID),
+			Rating:       ratingRequest.Rating,
+			Comment:      ratingRequest.Comment,
+		}
+
+		ratingData, err := rc.Repo.Create(data)
+		if err != nil {
+			ratingData, err = rc.Repo.Update(data)
+			if err != nil {
+				return c.JSON(http.StatusBadRequest, common.NewBadRequestResponse())
+			}
+		}
+
+		response := RatingResponse{
+			RestaurantID: int(ratingData.RestaurantID),
+			UserID:       int(ratingData.UserID),
+			Username:     ratingData.User.Name,
+			Rating:       ratingData.Rating,
+			Comment:      ratingData.Comment,
+		}
+
+		return c.JSON(http.StatusOK, response)
 	}
+}
 
-	uid := c.Get("user").(*jwt.Token)
-	claims := uid.Claims.(jwt.MapClaims)
-	userID := int(claims["userid"].(float64))
+func (rc RatingController) Update() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var ratingRequest UpdateRatingRequest
 
-	isCanGiveRating, _ := rc.Repo.IsCanGiveRating(userID, ratingRequest.RestaurantID)
-	if !isCanGiveRating {
-		return c.JSON(http.StatusNotFound, common.NewNotFoundResponse())
-	}
+		if err := c.Bind(&ratingRequest); err != nil {
+			return c.JSON(http.StatusBadRequest, common.NewBadRequestResponse())
+		}
 
-	data := entities.Rating{
-		RestaurantID: uint(ratingRequest.RestaurantID),
-		UserID:       uint(userID),
-		Rating:       ratingRequest.Rating,
-		Comment:      ratingRequest.Comment,
-	}
+		restaurantId, err := strconv.Atoi(c.Param("restaurantId"))
 
-	ratingData, err := rc.Repo.Create(data)
-	if err != nil {
-		ratingData, err = rc.Repo.Update(data)
 		if err != nil {
 			return c.JSON(http.StatusBadRequest, common.NewBadRequestResponse())
 		}
-	}
 
-	response := RatingResponse{
-		RestaurantID: int(ratingData.RestaurantID),
-		UserID:       int(ratingData.UserID),
-		Username:     ratingData.User.Name,
-		Rating:       ratingData.Rating,
-		Comment:      ratingData.Comment,
-	}
+		uid := c.Get("user").(*jwt.Token)
+		claims := uid.Claims.(jwt.MapClaims)
+		userID := int(claims["userid"].(float64))
 
-	return c.JSON(http.StatusOK, response)
+		data := entities.Rating{
+			RestaurantID: uint(restaurantId),
+			UserID:       uint(userID),
+			Rating:       ratingRequest.Rating,
+			Comment:      ratingRequest.Comment,
+		}
+
+		ratingData, err := rc.Repo.Update(data)
+		if err != nil {
+			return c.JSON(http.StatusNotFound, common.NewNotFoundResponse())
+		}
+
+		response := RatingResponse{
+			RestaurantID: int(ratingData.RestaurantID),
+			UserID:       int(ratingData.UserID),
+			Username:     ratingData.User.Name,
+			Rating:       ratingData.Rating,
+			Comment:      ratingData.Comment,
+		}
+
+		return c.JSON(http.StatusOK, response)
+	}
 }
 
-func (rc RatingController) Update(c echo.Context) error {
-	var ratingRequest UpdateRatingRequest
+func (rc RatingController) Delete() echo.HandlerFunc {
+	return func(c echo.Context) error {
 
-	if err := c.Bind(&ratingRequest); err != nil {
-		return c.JSON(http.StatusBadRequest, common.NewBadRequestResponse())
+		restaurantId, err := strconv.Atoi(c.Param("restaurantId"))
+
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, common.NewBadRequestResponse())
+		}
+
+		uid := c.Get("user").(*jwt.Token)
+		claims := uid.Claims.(jwt.MapClaims)
+		userID := int(claims["userid"].(float64))
+
+		_, err = rc.Repo.Delete(userID, restaurantId)
+		if err != nil {
+			return c.JSON(http.StatusNotFound, common.NewNotFoundResponse())
+		}
+
+		return c.JSON(http.StatusOK, common.NewSuccessOperationResponse())
 	}
-
-	restaurantId, err := strconv.Atoi(c.Param("restaurantId"))
-
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, common.NewBadRequestResponse())
-	}
-
-	uid := c.Get("user").(*jwt.Token)
-	claims := uid.Claims.(jwt.MapClaims)
-	userID := int(claims["userid"].(float64))
-
-	data := entities.Rating{
-		RestaurantID: uint(restaurantId),
-		UserID:       uint(userID),
-		Rating:       ratingRequest.Rating,
-		Comment:      ratingRequest.Comment,
-	}
-
-	ratingData, err := rc.Repo.Update(data)
-	if err != nil {
-		return c.JSON(http.StatusNotFound, common.NewNotFoundResponse())
-	}
-
-	response := RatingResponse{
-		RestaurantID: int(ratingData.RestaurantID),
-		UserID:       int(ratingData.UserID),
-		Username:     ratingData.User.Name,
-		Rating:       ratingData.Rating,
-		Comment:      ratingData.Comment,
-	}
-
-	return c.JSON(http.StatusOK, response)
-}
-
-func (rc RatingController) Delete(c echo.Context) error {
-
-	restaurantId, err := strconv.Atoi(c.Param("restaurantId"))
-
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, common.NewBadRequestResponse())
-	}
-
-	uid := c.Get("user").(*jwt.Token)
-	claims := uid.Claims.(jwt.MapClaims)
-	userID := int(claims["userid"].(float64))
-
-	_, err = rc.Repo.Delete(userID, restaurantId)
-	if err != nil {
-		return c.JSON(http.StatusNotFound, common.NewNotFoundResponse())
-	}
-
-	return c.JSON(http.StatusOK, common.NewSuccessOperationResponse())
 }
