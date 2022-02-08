bf228ac48873180507f5ce12cfaa0d7f0d970ff0
SunRise-11 <cleanpinesunrise03@gmail.com>
2022-02-08 10:40:29 +0800

    User
    + test unit repository 100% coverage
    + test unit controller 100% coverage

--- repository/users/users_test.go
+++ repository/users/users_test.go
@@ -11,10 +11,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUsersRepo(t *testing.T) {
+func TestRegisterAdminRepo(t *testing.T) {
 	config := configs.GetConfig()
 	db := utils.InitDB(config)
-
 	userRepo := NewUsersRepo(db)
 
 	t.Run("Register Admin", func(t *testing.T) {
@@ -30,63 +29,9 @@ func TestUsersRepo(t *testing.T) {
 		assert.Equal(t, res, res)
 		assert.Nil(t, err)
 	})
-
-	t.Run("Register User", func(t *testing.T) {
-		hash := sha256.Sum256([]byte("herlianto123"))
-		password := fmt.Sprintf("%x", hash[:])
-		var newUser entities.User
-		newUser.Name = "herlianto"
-		newUser.Email = "herlianto@outlook.my"
-		newUser.Password = password
-		newUser.PhoneNumber = "0877"
-
-		res, err := userRepo.Register(newUser)
-		assert.Equal(t, res, res)
-		assert.Nil(t, err)
-	})
-
-	t.Run("Show UserID 2", func(t *testing.T) {
-		res, err := userRepo.Get(2)
-		assert.Equal(t, res.ID, uint(2))
-		assert.Nil(t, err)
-	})
-
-	t.Run("Login UserID 2", func(t *testing.T) {
-		hash := sha256.Sum256([]byte("herlianto123"))
-		password := fmt.Sprintf("%x", hash[:])
-		var loginUser entities.User
-		loginUser.Email = "herlianto@outlook.my"
-		loginUser.Password = password
-
-		res, err := userRepo.LoginUser(loginUser.Email, loginUser.Password)
-		assert.Equal(t, res.ID, uint(2))
-		assert.Nil(t, err)
-	})
-
-	t.Run("Update UserID 2", func(t *testing.T) {
-		hash := sha256.Sum256([]byte("herlianto123"))
-		password := fmt.Sprintf("%x", hash[:])
-		var updateUser entities.User
-		updateUser.Name = "herlianto"
-		updateUser.Email = "herlianto@outlook.my"
-		updateUser.Password = password
-		updateUser.PhoneNumber = "0877"
-		updateUser.Balance = 100.000
-		updateUser.Reputation = 100
-
-		res, err := userRepo.Update(uint(2), updateUser)
-		assert.Equal(t, res.ID, uint(2))
-		assert.Nil(t, err)
-	})
-
-	t.Run("Delete UserID 2", func(t *testing.T) {
-		res, err := userRepo.Delete(2)
-		assert.Equal(t, res.ID, uint(2))
-		assert.Nil(t, err)
-	})
 }
 
-func TestFalseUsersRepo(t *testing.T) {
+func TestRegisterUsersRepo(t *testing.T) {
 	config := configs.GetConfig()
 	db := utils.InitDB(config)
 
@@ -96,7 +41,6 @@ func TestFalseUsersRepo(t *testing.T) {
 		hash := sha256.Sum256([]byte("herlianto123"))
 		password := fmt.Sprintf("%x", hash[:])
 		var newUser entities.User
-		newUser.ID = 3
 		newUser.Name = "herlianto"
 		newUser.Email = "herlianto@outlook.my"
 		newUser.Password = password
@@ -107,28 +51,56 @@ func TestFalseUsersRepo(t *testing.T) {
 		assert.Nil(t, err)
 	})
 
-	t.Run("FALSE Register User", func(t *testing.T) {
+	t.Run("ERROR Register User", func(t *testing.T) {
 		hash := sha256.Sum256([]byte("herlianto123"))
 		password := fmt.Sprintf("%x", hash[:])
 		var newUser entities.User
-		newUser.ID = 3
+		newUser.ID = 1
 		newUser.Name = "herlianto"
 		newUser.Email = "herlianto@outlook.my"
 		newUser.Password = password
 		newUser.PhoneNumber = "0877"
 
 		res, err := userRepo.Register(newUser)
-		assert.Equal(t, res.ID, uint(3))
+		assert.Equal(t, res, res)
 		assert.Error(t, err)
 	})
 
-	t.Run("FALSE Show UserID 4", func(t *testing.T) {
-		res, err := userRepo.Get(4)
-		assert.Equal(t, res.ID, uint(0))
-		assert.Error(t, err)
+}
+
+func TestLoginUsersRepo(t *testing.T) {
+	config := configs.GetConfig()
+	db := utils.InitDB(config)
+
+	userRepo := NewUsersRepo(db)
+
+	t.Run("Register User", func(t *testing.T) {
+		hash := sha256.Sum256([]byte("herlianto123"))
+		password := fmt.Sprintf("%x", hash[:])
+		var newUser entities.User
+		newUser.Name = "herlianto"
+		newUser.Email = "herlianto@outlook.my"
+		newUser.Password = password
+		newUser.PhoneNumber = "0877"
+
+		res, err := userRepo.Register(newUser)
+		assert.Equal(t, res, res)
+		assert.Nil(t, err)
 	})
 
-	t.Run("FALSE Login User", func(t *testing.T) {
+	t.Run("Login User", func(t *testing.T) {
+		hash := sha256.Sum256([]byte("herlianto123"))
+		password := fmt.Sprintf("%x", hash[:])
+		var loginUser entities.User
+		loginUser.Email = "herlianto@outlook.my"
+		loginUser.Password = password
+
+		res, err := userRepo.LoginUser(loginUser.Email, loginUser.Password)
+		assert.Equal(t, res.ID, uint(1))
+		assert.Nil(t, err)
+	})
+
+	t.Run("ERROR Login User", func(t *testing.T) {
 		hash := sha256.Sum256([]byte("herlianto1234"))
 		password := fmt.Sprintf("%x", hash[:])
 		var loginUser entities.User
@@ -139,8 +111,78 @@ func TestFalseUsersRepo(t *testing.T) {
 		assert.Equal(t, res.ID, uint(0))
 		assert.Error(t, err)
 	})
+}
 
-	t.Run("FALSE Update UserID 4", func(t *testing.T) {
+func TestDeleteUsersRepo(t *testing.T) {
+	config := configs.GetConfig()
+	db := utils.InitDB(config)
+
+	userRepo := NewUsersRepo(db)
+
+	t.Run("Register User", func(t *testing.T) {
+		hash := sha256.Sum256([]byte("herlianto123"))
+		password := fmt.Sprintf("%x", hash[:])
+		var newUser entities.User
+		newUser.Name = "herlianto"
+		newUser.Email = "herlianto@outlook.my"
+		newUser.Password = password
+		newUser.PhoneNumber = "0877"
+
+		res, err := userRepo.Register(newUser)
+		assert.Equal(t, res, res)
+		assert.Nil(t, err)
+	})
+
+	t.Run("Delete User", func(t *testing.T) {
+		res, err := userRepo.Delete(1)
+		assert.Equal(t, res.ID, uint(1))
+		assert.Nil(t, err)
+	})
+
+	t.Run("ERROR Delete User", func(t *testing.T) {
+		res, err := userRepo.Delete(1)
+		assert.Equal(t, res.ID, uint(0))
+		assert.Error(t, err)
+	})
+}
+
+func TestUpdateUsersRepo(t *testing.T) {
+	config := configs.GetConfig()
+	db := utils.InitDB(config)
+
+	userRepo := NewUsersRepo(db)
+
+	t.Run("Register User", func(t *testing.T) {
+		hash := sha256.Sum256([]byte("herlianto123"))
+		password := fmt.Sprintf("%x", hash[:])
+		var newUser entities.User
+		newUser.Name = "herlianto"
+		newUser.Email = "herlianto@outlook.my"
+		newUser.Password = password
+		newUser.PhoneNumber = "0877"
+
+		res, err := userRepo.Register(newUser)
+		assert.Equal(t, res, res)
+		assert.Nil(t, err)
+	})
+
+	t.Run("Update User", func(t *testing.T) {
+		hash := sha256.Sum256([]byte("herlianto123"))
+		password := fmt.Sprintf("%x", hash[:])
+		var updateUser entities.User
+		updateUser.Name = "herlianto"
+		updateUser.Email = "herlianto@outlook.my"
+		updateUser.Password = password
+		updateUser.PhoneNumber = "0877"
+		updateUser.Balance = 100.000
+		updateUser.Reputation = 100
+
+		res, err := userRepo.Update(uint(1), updateUser)
+		assert.Equal(t, res.ID, uint(1))
+		assert.Nil(t, err)
+	})
+
+	t.Run("ERROR Update User", func(t *testing.T) {
 		hash := sha256.Sum256([]byte("herlianto123"))
 		password := fmt.Sprintf("%x", hash[:])
 		var updateUser entities.User
@@ -155,9 +197,36 @@ func TestFalseUsersRepo(t *testing.T) {
 		assert.Equal(t, res.ID, uint(0))
 		assert.Error(t, err)
 	})
+}
 
-	t.Run("FALSE Delete UserID 4", func(t *testing.T) {
-		res, err := userRepo.Delete(4)
+func TestGetUsersRepo(t *testing.T) {
+	config := configs.GetConfig()
+	db := utils.InitDB(config)
+
+	userRepo := NewUsersRepo(db)
+
+	t.Run("Register User", func(t *testing.T) {
+		hash := sha256.Sum256([]byte("herlianto123"))
+		password := fmt.Sprintf("%x", hash[:])
+		var newUser entities.User
+		newUser.Name = "herlianto"
+		newUser.Email = "herlianto@outlook.my"
+		newUser.Password = password
+		newUser.PhoneNumber = "0877"
+
+		res, err := userRepo.Register(newUser)
+		assert.Equal(t, res, res)
+		assert.Nil(t, err)
+	})
+
+	t.Run("Get User", func(t *testing.T) {
+		res, err := userRepo.Get(uint(1))
+		assert.Equal(t, res.ID, uint(1))
+		assert.Nil(t, err)
+	})
+
+	t.Run("ERROR Get User", func(t *testing.T) {
+		res, err := userRepo.Get(uint(2))
 		assert.Equal(t, res.ID, uint(0))
 		assert.Error(t, err)
 	})
